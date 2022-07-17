b89ba4b72fb308cf24af256ef4352e514c22483d
Kevin Gut <kevin.gut@bitm.sg>
2022-07-17 22:29:02 +0200

    Add form

--- ServiceMonitor/FrmMain.Designer.cs
+++ ServiceMonitor/FrmMain.Designer.cs
@@ -29,10 +29,17 @@ namespace ServiceMonitor
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Name = "FrmMain";
+            this.Text = "Service Monitor";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
