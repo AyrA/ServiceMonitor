4693897759868e8feac28db1a49e10a8a5b68f7f
Kevin Gut <kevin.gut@bitm.sg>
2022-07-24 11:24:38 +0200

    UI

--- ServiceMonitor/FrmMain.Designer.cs
+++ ServiceMonitor/FrmMain.Designer.cs
@@ -29,20 +29,185 @@ namespace ServiceMonitor
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuTestAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuCloseWindow = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuAddPlugin = new System.Windows.Forms.ToolStripMenuItem();
+            this.LvPlugins = new System.Windows.Forms.ListView();
+            this.ChName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ChLastResult = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ChNextCheck = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Cms = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CmsTestItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CmsConfigItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CmsEnableItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CmsDeleteItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainMenu.SuspendLayout();
+            this.Cms.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.MnuAddPlugin});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(584, 24);
+            this.MainMenu.TabIndex = 0;
+            this.MainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MnuTestAll,
+            this.MnuCloseWindow,
+            this.MnuExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // MnuTestAll
+            // 
+            this.MnuTestAll.Name = "MnuTestAll";
+            this.MnuTestAll.Size = new System.Drawing.Size(139, 22);
+            this.MnuTestAll.Text = "&Test all";
+            this.MnuTestAll.Click += new System.EventHandler(this.MnuTestAll_Click);
+            // 
+            // MnuCloseWindow
+            // 
+            this.MnuCloseWindow.Name = "MnuCloseWindow";
+            this.MnuCloseWindow.Size = new System.Drawing.Size(139, 22);
+            this.MnuCloseWindow.Text = "&Close window";
+            this.MnuCloseWindow.Click += new System.EventHandler(this.MnuCloseWindow_Click);
+            // 
+            // MnuExit
+            // 
+            this.MnuExit.Name = "MnuExit";
+            this.MnuExit.Size = new System.Drawing.Size(139, 22);
+            this.MnuExit.Text = "&Exit";
+            this.MnuExit.Click += new System.EventHandler(this.MnuExit_Click);
+            // 
+            // MnuAddPlugin
+            // 
+            this.MnuAddPlugin.Name = "MnuAddPlugin";
+            this.MnuAddPlugin.Size = new System.Drawing.Size(70, 20);
+            this.MnuAddPlugin.Text = "&Add Plugin";
+            // 
+            // LvPlugins
+            // 
+            this.LvPlugins.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LvPlugins.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ChName,
+            this.ChLastResult,
+            this.ChNextCheck});
+            this.LvPlugins.ContextMenuStrip = this.Cms;
+            this.LvPlugins.FullRowSelect = true;
+            this.LvPlugins.HideSelection = false;
+            this.LvPlugins.Location = new System.Drawing.Point(12, 37);
+            this.LvPlugins.MultiSelect = false;
+            this.LvPlugins.Name = "LvPlugins";
+            this.LvPlugins.Size = new System.Drawing.Size(560, 312);
+            this.LvPlugins.TabIndex = 1;
+            this.LvPlugins.UseCompatibleStateImageBehavior = false;
+            this.LvPlugins.View = System.Windows.Forms.View.Details;
+            this.LvPlugins.SelectedIndexChanged += new System.EventHandler(this.LvPlugins_SelectedIndexChanged);
+            this.LvPlugins.DoubleClick += new System.EventHandler(this.LvPlugins_DoubleClick);
+            this.LvPlugins.KeyDown += new System.Windows.Forms.KeyEventHandler(this.LvPlugins_KeyDown);
+            // 
+            // ChName
+            // 
+            this.ChName.Text = "Name";
+            this.ChName.Width = 170;
+            // 
+            // ChLastResult
+            // 
+            this.ChLastResult.Text = "Last Result";
+            this.ChLastResult.Width = 170;
+            // 
+            // ChNextCheck
+            // 
+            this.ChNextCheck.Text = "NextCheck";
+            this.ChNextCheck.Width = 170;
+            // 
+            // Cms
+            // 
+            this.Cms.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CmsTestItem,
+            this.CmsConfigItem,
+            this.CmsEnableItem,
+            this.CmsDeleteItem});
+            this.Cms.Name = "Cms";
+            this.Cms.Size = new System.Drawing.Size(108, 92);
+            // 
+            // CmsTestItem
+            // 
+            this.CmsTestItem.Name = "CmsTestItem";
+            this.CmsTestItem.Size = new System.Drawing.Size(180, 22);
+            this.CmsTestItem.Text = "&Test";
+            this.CmsTestItem.Click += new System.EventHandler(this.CmsTestItem_Click);
+            // 
+            // CmsConfigItem
+            // 
+            this.CmsConfigItem.Name = "CmsConfigItem";
+            this.CmsConfigItem.Size = new System.Drawing.Size(180, 22);
+            this.CmsConfigItem.Text = "&Config";
+            this.CmsConfigItem.Click += new System.EventHandler(this.CmsConfigItem_Click);
+            // 
+            // CmsEnableItem
+            // 
+            this.CmsEnableItem.Name = "CmsEnableItem";
+            this.CmsEnableItem.Size = new System.Drawing.Size(180, 22);
+            this.CmsEnableItem.Text = "&Enable";
+            this.CmsEnableItem.Click += new System.EventHandler(this.CmsEnableItem_Click);
+            // 
+            // CmsDeleteItem
+            // 
+            this.CmsDeleteItem.Name = "CmsDeleteItem";
+            this.CmsDeleteItem.Size = new System.Drawing.Size(180, 22);
+            this.CmsDeleteItem.Text = "&Delete";
+            this.CmsDeleteItem.Click += new System.EventHandler(this.CmsDeleteItem_Click);
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.LvPlugins);
+            this.Controls.Add(this.MainMenu);
+            this.MainMenuStrip = this.MainMenu;
             this.Name = "FrmMain";
             this.Text = "Service Monitor";
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            this.Cms.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MnuTestAll;
+        private System.Windows.Forms.ToolStripMenuItem MnuCloseWindow;
+        private System.Windows.Forms.ToolStripMenuItem MnuExit;
+        private System.Windows.Forms.ToolStripMenuItem MnuAddPlugin;
+        private System.Windows.Forms.ListView LvPlugins;
+        private System.Windows.Forms.ColumnHeader ChName;
+        private System.Windows.Forms.ColumnHeader ChLastResult;
+        private System.Windows.Forms.ColumnHeader ChNextCheck;
+        private System.Windows.Forms.ContextMenuStrip Cms;
+        private System.Windows.Forms.ToolStripMenuItem CmsTestItem;
+        private System.Windows.Forms.ToolStripMenuItem CmsConfigItem;
+        private System.Windows.Forms.ToolStripMenuItem CmsEnableItem;
+        private System.Windows.Forms.ToolStripMenuItem CmsDeleteItem;
     }
 }
 
--- ServiceMonitor/FrmMain.cs
+++ ServiceMonitor/FrmMain.cs
@@ -1,12 +1,290 @@
-﻿using System.Windows.Forms;
+﻿using ServiceMonitor.Properties;
+using System;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace ServiceMonitor
 {
     public partial class FrmMain : Form
     {
+        private const int LED_OFF = 0;
+        private const int LED_OK = LED_OFF + 1;
+        private const int LED_WARN = LED_OK + 1;
+        private const int LED_ERR = LED_WARN + 1;
+
         public FrmMain()
         {
             InitializeComponent();
+            LvPlugins.SmallImageList = new ImageList
+            {
+                ColorDepth = ColorDepth.Depth32Bit,
+                ImageSize = new System.Drawing.Size(16, 16)
+            };
+            LvPlugins.SmallImageList.Images.Add(Resources.LedCheckOff);
+            LvPlugins.SmallImageList.Images.Add(Resources.LedCheckOk);
+            LvPlugins.SmallImageList.Images.Add(Resources.LedCheckWarning);
+            LvPlugins.SmallImageList.Images.Add(Resources.LedCheckError);
+            ShowPlugins();
         }
+
+        public void SetMenuPlugins(PluginInfo[] infos)
+        {
+            MnuAddPlugin.DropDownItems.Clear();
+            foreach (var info in infos)
+            {
+                var item = MnuAddPlugin.DropDownItems.Add(info.BaseName);
+                item.Tag = info;
+                item.Click += AddTest;
+            }
+        }
+
+        public void ShowPlugins()
+        {
+            LvPlugins.SuspendLayout();
+            ClearEvents();
+            LvPlugins.Items.Clear();
+            foreach (var PI in PluginManager.Plugins.OrderBy(m => m.Plugin.PluginType.FullName).ThenBy(m => m.Plugin.Name))
+            {
+                var Entry = LvPlugins.Items.Add(PI.Plugin.Name);
+                Entry.Tag = PI;
+                Entry.SubItems.Add(string.Empty);
+                Entry.SubItems.Add(string.Empty);
+                UpdateListItem(Entry, PI);
+                PI.TestComplete += PluginTestComplete;
+            }
+            LvPlugins.ResumeLayout();
+        }
+
+        private void AddTest(object sender, EventArgs e)
+        {
+            if (!(sender is ToolStripItem Item))
+            {
+                throw new ArgumentException($"Expected {nameof(ToolStripItem)} but got {sender.GetType().FullName}");
+            }
+            var PI = (PluginInfo)Item.Tag;
+            var P = PluginManager.LoadPlugin(PI);
+            if (P.Config())
+            {
+                PluginManager.Plugins.Add(new PluginStatus(P));
+                P.Start();
+                PluginManager.SavePluginData();
+                ShowPlugins();
+            }
+            else
+            {
+                P.Dispose();
+            }
+        }
+
+        private void ClearEvents()
+        {
+            foreach (var Item in LvPlugins.Items.OfType<ListViewItem>())
+            {
+                ((PluginStatus)Item.Tag).TestComplete -= PluginTestComplete;
+            }
+        }
+
+        private void UpdateListItem(ListViewItem item, PluginStatus info)
+        {
+            item.Text = info.Plugin.Name;
+            if (info.Enabled)
+            {
+                item.SubItems[1].Text = info.LastError == null ? "OK" : info.LastError.Message;
+                item.SubItems[2].Text = info.Plugin.NextCheck.ToLocalTime().ToString("G");
+            }
+            else
+            {
+                item.SubItems[1].Text = "Disabled";
+                item.SubItems[2].Text = "<None>";
+            }
+            if (!info.Enabled)
+            {
+                item.ImageIndex = LED_OFF;
+            }
+            else
+            {
+                if (info.IsTesting)
+                {
+                    item.ImageIndex = LED_WARN;
+                }
+                else
+                {
+                    item.ImageIndex = info.LastError == null ? LED_OK : LED_ERR;
+                }
+            }
+        }
+
+        private void AskDelete(ListViewItem item)
+        {
+            var info = (PluginStatus)item.Tag;
+            if (MessageBox.Show("Delete this plugin?", "Delete " + info.Plugin.Name, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                item.Remove();
+                PluginManager.Plugins.Remove(info);
+                info.TestComplete -= PluginTestComplete;
+                PluginManager.SavePluginData();
+            }
+        }
+
+        #region Event handler
+
+        private void MnuTestAll_Click(object sender, EventArgs e)
+        {
+            foreach (var PI in PluginManager.Plugins)
+            {
+                try
+                {
+                    PI.BeginTest(false);
+                }
+                catch
+                {
+                    //NOOP
+                }
+            }
+        }
+
+        private void MnuCloseWindow_Click(object sender, EventArgs e)
+        {
+            ClearEvents();
+            Close();
+        }
+
+        private void MnuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PluginTestComplete(PluginStatus sender, Exception previousResult)
+        {
+            //Run event results in UI thread
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    PluginTestComplete(sender, previousResult);
+                });
+                return;
+            }
+            var ErrorChange = false;
+            if ((sender.LastError == null && previousResult != null) ||
+                (sender.LastError != null && previousResult == null))
+            {
+                ErrorChange = true;
+            }
+            if (ErrorChange)
+            {
+                if (sender.LastError == null)
+                {
+                    Program.ShowInfo("Error condition resolved", sender.Plugin.Name, ToolTipIcon.Info);
+                }
+                else
+                {
+                    Program.ShowInfo($"Service status alert:\r\n{sender.LastError.Message}", sender.Plugin.Name, ToolTipIcon.Error);
+                }
+            }
+            var ListItem = LvPlugins.Items.OfType<ListViewItem>().FirstOrDefault(m => ((PluginStatus)m.Tag) == sender);
+            if (ListItem != null)
+            {
+                UpdateListItem(ListItem, sender);
+            }
+            else
+            {
+                sender.TestComplete -= PluginTestComplete;
+            }
+        }
+
+        private void LvPlugins_DoubleClick(object sender, EventArgs e)
+        {
+            if (LvPlugins.SelectedItems.Count > 0)
+            {
+                var item = LvPlugins.SelectedItems[0];
+                var info = (PluginStatus)item.Tag;
+                if (info.Plugin.Config())
+                {
+                    PluginManager.SavePluginData();
+                    UpdateListItem(item, info);
+                }
+            }
+        }
+
+        private void LvPlugins_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (LvPlugins.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            var item = LvPlugins.SelectedItems[0];
+            var info = (PluginStatus)item.Tag;
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    if (info.Plugin.Config())
+                    {
+                        PluginManager.SavePluginData();
+                        UpdateListItem(item, info);
+                    }
+                    break;
+                case Keys.Delete:
+                    AskDelete(item);
+                    break;
+            }
+        }
+
+        private void CmsTestItem_Click(object sender, EventArgs e)
+        {
+            var Item = LvPlugins.SelectedItems.OfType<ListViewItem>().FirstOrDefault();
+            if (Item != null)
+            {
+                var Plugin = (PluginStatus)Item.Tag;
+                Plugin.BeginTest(false);
+            }
+        }
+
+        private void CmsConfigItem_Click(object sender, EventArgs e)
+        {
+            var Item = LvPlugins.SelectedItems.OfType<ListViewItem>().FirstOrDefault();
+            if (Item != null)
+            {
+                var Plugin = (PluginStatus)Item.Tag;
+                if (Plugin.Plugin.Config())
+                {
+                    PluginManager.SavePluginData();
+                    UpdateListItem(Item, Plugin);
+                }
+            }
+        }
+
+        private void CmsEnableItem_Click(object sender, EventArgs e)
+        {
+            var Item = LvPlugins.SelectedItems.OfType<ListViewItem>().FirstOrDefault();
+            if (Item != null)
+            {
+                var Plugin = (PluginStatus)Item.Tag;
+                Plugin.Enabled = !Plugin.Enabled;
+                CmsEnableItem.Checked = !CmsEnableItem.Checked;
+                UpdateListItem(Item, Plugin);
+                PluginManager.SavePluginData();
+            }
+        }
+
+        private void CmsDeleteItem_Click(object sender, EventArgs e)
+        {
+            var Item = LvPlugins.SelectedItems.OfType<ListViewItem>().FirstOrDefault();
+            if (Item != null)
+            {
+                AskDelete(Item);
+            }
+        }
+
+        private void LvPlugins_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var Item = LvPlugins.SelectedItems.OfType<ListViewItem>().FirstOrDefault();
+            if (Item != null)
+            {
+                CmsEnableItem.Checked = ((PluginStatus)Item.Tag).Enabled;
+            }
+        }
+
+        #endregion
     }
 }
